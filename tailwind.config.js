7d48bcf07fe60569655e0feeac6a49ba6df9dc72
harbanery <ryusuf05@gmail.com>
2024-08-12 12:03:35 +0700

    updating tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         "./resources/**/*.js",
         "./resources/**/*.vue",
         "./node_modules/flowbite/**/*.js",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
     ],
     safelist: ["bg-red-100", "bg-green-100", "bg-blue-100", "bg-yellow-100"],
     theme: {
